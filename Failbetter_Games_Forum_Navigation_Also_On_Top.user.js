80524a231fc80bf42e0e93c80e91624b37bf176b
Laurvin <github@dbbv.com>
2017-06-22 23:29:56 +0200

    Update Failbetter_Games_Forum_Navigation_Also_On_Top.user.js

--- Failbetter_Games_Forum_Navigation_Also_On_Top.user.js
+++ Failbetter_Games_Forum_Navigation_Also_On_Top.user.js
@@ -5,7 +5,7 @@
 // @description Copies the paging to the top of the page.
 // @version 1
 // @icon http://i.imgur.com/XYzKXzK.png
-// @downloadURL xxx
+// @downloadURL https://github.com/Laurvin/Failbetter-Games-Forum-Navigation-Also-On-Top/raw/master/Failbetter_Games_Forum_Navigation_Also_On_Top.user.js
 // @include http://community.failbettergames.com/*
 // @include https://community.failbettergames.com/*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
